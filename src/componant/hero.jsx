06aad6d592fdc5d85899b90bb33a55a0570e894e
Mazen slime <mazenslime16@gmail.com>
2026-08-05 16:52:53 +0300

    new commet latest

--- src/componant/hero.jsx
+++ src/componant/hero.jsx
@@ -13,23 +13,10 @@ const Hero = ({ Width }) => {
             className="rounded-lg shadow-lg position-relative object-cover"
         />
         <div className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center cursor-pointer ${Width > 900 ? 'w-150' : 'w-full'}`}>
-            <motion.h1      
-            initial={{opacity:0}}
-            animate={{opacity:1}}
-            // whileHover={{ border: '2px solid white' }}
-            transition={{ duration: 1 ,delay: 1.5}}
-            className= {` ${Width > 900 ? 'text-7xl' : 'text-5xl'} font-bold text-white mb-6 p-4`}>Web Devolopment</motion.h1>
-            <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1}}
-            // whileHover={{ border: '2px solid white' }}
-            transition={{ duration: 1,delay:1.6}}
+            <h1 className= {` ${Width > 900 ? 'text-7xl' : 'text-5xl'} font-bold text-white mb-6 p-4`}>Web Devolopment</motion.h1>
+            <p
              className={ ` ${Width > 900 ? 'text-7xl' : 'text-5xl'} font-bold text-white p-4`}>and Desine stiduio </motion.p>
-            <motion.p 
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            // whileHover={{ border: '2px solid white' }}
-            transition={{ duration: 1,delay:1.7 }}
+            <p 
             className={ ` ${Width > 900 ? 'text-7xl' : 'text-5xl'} font-bold text-white p-4`}>From the future</motion.p>
         </div>
     </div>
