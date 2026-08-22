99d6eb52081ac8dc74f982be4e812bf24857686d
Mazen slime <mazenslime16@gmail.com>
2026-08-22 15:18:57 +0300

    newcommet

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {preload} from 'react-dom';
+import { preconnect } from 'react-dom';
 const Component=async()=>{
-  preload('./assets/لقطة الشاشة 2026-02-26 171531.webp',{ as: 'image', type: 'image/webp'});
-  preload('./assets/لقطة الشاشة 2026-02-27 000723.webp',{ as: 'image', type: 'image/webp'});
-  preload('./assets/لقطة الشاشة 2026-02-27 001650.webp',{ as: 'image', type: 'image/webp'});
-  preload('./assets/photo.webp',{ as: 'image', type: 'image/webp'})
+  preconnect('./assets/لقطة الشاشة 2026-02-26 171531.webp',{ as: 'image', type: 'image/webp'});
+  preconnect('./assets/لقطة الشاشة 2026-02-27 000723.webp',{ as: 'image', type: 'image/webp'});
+  preconnect('./assets/لقطة الشاشة 2026-02-27 001650.webp',{ as: 'image', type: 'image/webp'});
+  preconnect('./assets/photo.webp',{ as: 'image', type: 'image/webp'})
 }
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { analyzer } from 'vite-bundle-analyzer'
-
+import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression'
 export default defineConfig({
-  plugins: [react(), tailwindcss(),analyzer()],
+  plugins: [react(), tailwindcss(),analyzer(), viteCompression()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
